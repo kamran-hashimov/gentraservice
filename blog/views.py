e24d9d263ab2f5ee8b37869d1539908b6c85679c
kamran <kheshimov@gmail.com>
2022-02-21 19:46:16 +0400

    There are some new changes

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 
-def home(self):
-    return HttpResponse("tema")
+def post_list(request):
+    return render(request,'blog/post_list.html', {})
--- gentraservice/settings.py
+++ gentraservice/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'gentraservice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
